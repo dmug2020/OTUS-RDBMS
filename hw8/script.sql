1161c5d366c8200c5a0e531ef0b721a089a84007
dimas <json1@yandex.ru>
2019-06-27 23:42:52 +0700

    Add files via upload

---
+++ hw8/script.sql
@@ -0,0 +1,49 @@
+use blog;
+
+-- ¬ставка данных
+insert into posts (post_title, post_content)
+  values ('роман о молоке', 'длинный при длинный текст о молоке');
+insert into posts (post_title, post_content)
+  values ('роман о пиве ', 'длинный при длинный текст о пиве');
+insert into posts (post_title, post_content)
+  values ('роман о водке ', 'длинный при длинный текст о водке');
+
+-- ¬ставка с использованием select (во временную таблицу вставл€ем статистику по лайкам,прочтени€м,репостам,комментари€м где статистика по лайкам > 100)
+
+INSERT INTO
+    `temp_statistic` (`post_likes`,`post_reads`,`post_repost`,`post_comments`)
+SELECT
+    `src`.`post_likes`,
+    `src`.`post_reads`,
+    `src`.`post_repost` ,
+	`src`.`post_comments`
+FROM
+    `posts`  AS `src`
+WHERE
+    `src`.`post_likes`  > 100;
+	
+
+-- ќбновление данных
+update posts set post_title='роман о лимонаде' where post_comments=1;
+
+-- обновл€ем таблицу POSTS,значение записи Post_likes присваеваем всем постам = 1000 где никнейм автора = supernikname
+update `posts` as p
+  join users u on p.user_id = u.user_id 
+  set p.post_likes=1000
+  where u.display_name=(select display_name from users where display_name'supernikname');
+
+ 
+  
+-- ”даление
+delete from photos where created_at BETWEEN '2019-03-02|09:02:20' AND '2019-03-02|09:02:40'
+
+-- ¬ставка и обновление
+insert into posts (post_title, post_contents)
+  values ('это загловок', 'длинный текст') on duplicate key update post_id=post_id+1;
+
+
+-- ќчистка данных
+
+delete from posts;
+delete from photos;
+delete from temp_statistic;
